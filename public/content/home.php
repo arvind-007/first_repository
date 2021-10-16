f2f97699373a828f6ac1f82df723541612993877
manish-patodiya <mpatodiya2@gmail.com>
2021-10-16 17:14:50 +0530

    create HomePage

--- public/content/home.php
+++ public/content/home.php
@@ -1,2 +1,36 @@
+<section>
+    <div>
+        <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg">
+    </div>
+    <div class="circle">
+        <div>
+            <li class="circle1">
+                <a href="#" class="fab fa-windows fa-5x "></a>
+            </li>
+            <p>
+            <h2>Window</h2>
+            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque maxime necessitatibus magni repellat voluptatem laboriosam amet non consequatur facere doloribus atque sunt hic delectus aperiam ut aliquam odit eveniet at, molestiae quae."
+            </p>
+        </div>
+        <div>
+            <li>
+                <a href="#" class="fab fa-apple fa-5x "></a>
 
-     <section>       This is my Home page </section>
+            </li>
+            <p>
+            <h2>ios</h2>
+            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque maxime necessitatibus magni repellat voluptatem laboriosam amet non consequatur facere doloribus atque sunt hic delectus aperiam ut aliquam odit eveniet at, molestiae quae."
+            </p>
+        </div>
+        <div>
+            <li>
+                <a href="#" class="fab fa-linux fa-5x "></a>
+            </li>
+            <p>
+            <h2>linux</h2>
+            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque maxime necessitatibus magni repellat voluptatem laboriosam amet non consequatur facere doloribus atque sunt hic delectus aperiam ut aliquam odit eveniet at, molestiae quae."
+            </p>
+        </div>
+    </div>
+</section>
+<br><br><br>
