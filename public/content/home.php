1963151cd5109ed1939a56a3666e0513708569de
manish-patodiya <mpatodiya2@gmail.com>
2021-10-18 10:24:27 +0530

    change in home page

--- public/content/home.php
+++ public/content/home.php
@@ -3,45 +3,42 @@
         Aarambh
     </div>
     <center>
-    <div class="circle">
-        <div>
+        <div class="circle">
+            <div>
                 <li>
                     <a href="https://www.microsoft.com/en-in/windows/" class="fab fa-windows fa-5x "></a>
                 </li>
-
-                <p>
                 <h2>Window</h2>
-                "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque maxime necessitatibus magni repellat
-                voluptatem laboriosam amet non consequatur facere doloribus atque sunt hic delectus aperiam ut aliquam
-                odit eveniet at, molestiae quae."
+                <p>
+                    "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, nisi hic suscipit labore ut nulla
+                    enim reiciendis placeat inventore consectetur natus repellat architecto ipsam laudantium in? Iure
+                    possimus non tempore repudiandae nemo in, odit minima esse maiores."
                 </p>
-        </div>
-        <div>
+            </div>
+            <div>
                 <li>
                     <a href="https://developer.apple.com/ios/" class="fab fa-apple fa-5x "></a>
                 </li>
 
-                <p>
                 <h2>ios</h2>
-                "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque maxime necessitatibus magni repellat
-                voluptatem laboriosam amet non consequatur facere doloribus atque sunt hic delectus aperiam ut aliquam
-                odit
-                eveniet at, molestiae quae."
+                <p>
+                   "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione, tenetur at dolor, ex vero
+                    expedita maiores unde hic odit architecto impedit veritatis sit fuga quod error minus dolorum
+                    quaerat nulla autem praesentium, laboriosam atque distinctio id. Non!"
                 </p>
-        </div>
-        <div>
+            </div>
+            <div>
                 <li>
                     <a href="https://www.linux.org/" class="fab fa-linux fa-5x "></a>
                 </li>
-                
-                <p>
+
                 <h2>linux</h2>
-                "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque maxime necessitatibus magni repellat
-                voluptatem laboriosam amet non consequatur facere doloribus atque sunt hic delectus aperiam ut aliquam
-                odit
-                eveniet at, molestiae quae."
+                <p>
+                    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum expedita aut ut molestiae veniam
+                    distinctio, quis suscipit quo repudiandae! Quidem officiis porro aperiam aliquam eos repudiandae
+                    autem enim, quia, corporis, laboriosam saepe fuga nobis. Sed, fugiat pariatur."
                 </p>
+            </div>
         </div>
-    </div>
     </center>
 </section>
