7a193140d2c3505edf4ae18239f075eba1ce6b88
Rahulsingh0106 <rahulsingh01062000@gmail.com>
2021-10-19 15:28:20 +0530

    new changes

--- public/content/contact.php
+++ public/content/contact.php
@@ -1,62 +1,51 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <title>Document</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- Bootstrap CSS  -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
-    <title>Hello, world!</title>
-</head>
-
-<body>
-    <section class="main_heading my-5">
-        <div class="text-center">
-            <h1 class="display-4">Contact Us</h1>
-            <hr class="w-25 mx-auto">
-        </div>
-        <div class="container1">
-            <div class="row mx-4" >
-              <div class="col">
-              <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d113893.29524512296!2d75.73667839999999!3d26.866483199999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1634625329428!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
-              </div>
-              <div class="col">
-              <form>
+<section class="main_heading my-5">
+    <div class="text-center">
+        <h1 class="display-4">Contact Us</h1>
+        <hr class="w-25 mx-auto">
+    </div>
+    <div class="container1">
+        <div class="row mx-4">
+            <div class="col">
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d113893.29524512296!2d75.73667839999999!3d26.866483199999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1634625329428!5m2!1sen!2sin"
+                    width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+            </div>
+            <div class="col">
+                <form>
                     <div class="col-xxl-3 col-10  col-md-8 mx-auto">
                         <div class="mb-3">
                             <label for="exampleInputPassword1" class="form-label">Name</label>
                             <div class="input-group flex-nowrap">
-  <span class="input-group-text" id="addon-wrapping"><i class="fas fa-user"></i></span>
-  <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="addon-wrapping">
-</div>
+                                <span class="input-group-text" id="addon-wrapping"><i class="fas fa-user"></i></span>
+                                <input type="text" class="form-control" placeholder="Username" aria-label="Username"
+                                    aria-describedby="addon-wrapping">
+                            </div>
 
                         </div>
                         <div class="mb-3">
                             <label for="exampleInputEmail1" class="form-label">Email Address</label>
                             <span class="input-group-text" id="addon-wrapping"><i class="far fa-envelope"></i></span>
-  <input type="text" class="form-control" placeholder="....@gmil.com" aria-label="Username" aria-describedby="addon-wrapping">
-</div> 
-                            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                            <input type="text" class="form-control" placeholder="....@gmil.com" aria-label="Username"
+                                aria-describedby="addon-wrapping">
                         </div>
-                        <center
-                      <div class="mb-3 my-4">
-                       <button type="button" class="btn btn-warning">Submit</button></div></center>
-                            
-
-                    </form>
-                </div>
+                        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                    </div>
+                    <center <div class="mb-3 my-4">
+                        <button type="button" class="btn btn-warning">Submit</button>
             </div>
+            </center>
+
+
+            </form>
         </div>
-    </section>
+    </div>
+    </div>
+</section>
 
 
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script> 
-
-
-</body>
-
-</html>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"
+    integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous">
+</script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
+    integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous">
+</script>
