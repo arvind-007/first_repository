4c59f81945c1084c24d66ec530746e913acfac23
Rahulsingh0106 <rahulsingh01062000@gmail.com>
2021-10-19 09:10:46 +0530

    new changes 2

--- public/content/contact.php
+++ public/content/contact.php
@@ -3,39 +3,90 @@
 
 <head>
     <title>Document</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS  -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> 
+
+    <title>Hello, world!</title>
 </head>
-
 <body>
-<section1>
+  <section class="main_heading my-5">
+    <div class="text-center">
+      <h1 class="display-4">Contact Us</h1>
+      <hr class="w-25 mx-auto">
+  </div>
+  <div class="container1">
+    <div class="row">
+      <div class="col-xxl-8 col-10  col-md-8 mx-auto">
+      <form>
 
-<div class="div1">
-    <img src="./img/map.png">
-</div>        
-<div class="div2">
-    <h2>Address</h2>
-    <Address>
-        Nawalgarh, Rajasthan (Jhunjhunu)
-        Ghumchakar Road
-    </Address>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Name</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Enter your name">
+  
 </div>
-<div class="div3">
-    <h2>Contact Us </h2>
-    Rahul Singh :- 6376538745
-</div>        
+<div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email Address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div class="mb-3">
+    <label class="pr-4">Gender:</label>
+  <div class="form-check form-check-inline">
+  <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
+  <label class="form-check-label" for="inlineCheckbox1">Male</label>
+</div>
+<div class="form-check form-check-inline">
+  <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
+  <label class="form-check-label" for="inlineCheckbox2">Female</label>
+</div>
 
-</section1>
-<section2>
-<div class="div4">
-
-<form >
-    <label>Name :</label><input type="text"><br><br>
-    <label>Contact No. :</label> <input type="text"><br><br>
-    <label>Message :</label><br><br>
-    <textarea rows="4" columns="50"></textarea><br><br>
+</div>
+  <label for="exampleDataList" class="form-label">Select City</label>
+<input class="form-control" list="datalistOptions" id="exampleDataList" placeholder="Type to search...">
+<datalist id="datalistOptions">
+  <option value="San Francisco"></option>
+  <option value="New York"></option>
+  <option value="Seattle"></option>
+  <option value="Los Angeles"></option>
+  <option value="Chicago"></option>
+</datalist>
+  <div class="mb-3">
+  <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+  </div>
+  <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
 </form>
-<button>Submit</button>
 </div>
-</section2>
+</div>
+</div>
+  </section>
+<section class="additional_features mx-5 my-5">
+
+<div class="card" style="width: 18rem;">
+  <div class="card-body "> 
+    <h5 class="card-title">Call Us</h5>
+    <h6 class="card-subtitle mb-2 text-muted">Phone Number</h6>
+    <p class="card-text">Rahul Singh :- 6376538745</p>
+</div></div>
+
+<div class="card" style="width: 18rem; mx-5 ">
+  <div class="card-body ">
+    <h5 class="card-title">Our Address</h5>
+    <h6 class="card-subtitle mb-2 text-muted"Local Address></h6>
+    <p class="card-text">Nawalgarh, Rajasthan (Jhunujhunu) Ghumchakar</p>
+</div>
+</div>
+</section>
+    <!-- <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script> -->
+
+    
 </body>
 
 </html>
