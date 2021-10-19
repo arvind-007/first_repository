2ff44720b1d4a52af547a0a64eb2f4d778ff41db
manish-patodiya <mpatodiya2@gmail.com>
2021-10-19 16:05:58 +0530

    update home page

--- public/header/header.php
+++ public/header/header.php
@@ -1,26 +1,26 @@
 <?php $base_url = 'localhost/first_repository/public/'; ?>
 <header class="p-3 bg-dark text-white">
     <div class="container">
-      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-        <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-          <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"/></svg>
-        </a>
+        <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+            <img src="./img/logo.png" style=" width: 6%; margin-top: -15px; margin-bottom: -15px;">
+            <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+            </a>
 
-        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-          <li><a href="./index.php" class="nav-link px-2 text-secinondary">Home</a></li>
-          <li><a href="./about.php" class="nav-link px-2 text-white">About us</a></li>
-          <li><a href="./services.php" class="nav-link px-2 text-white">Services</a></li>
-          <li><a href="./contact.php" class="nav-link px-2 text-white">Contact us</a></li>
-        </ul>
+            <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                <li><a href="./index.php" class="nav-link px-2 text-secinondary">Home</a></li>
+                <li><a href="./about.php" class="nav-link px-2 text-white">About us</a></li>
+                <li><a href="./services.php" class="nav-link px-2 text-white">Services</a></li>
+                <li><a href="./contact.php" class="nav-link px-2 text-white">Contact us</a></li>
+            </ul>
 
-        <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
-          <input type="search" class="form-control form-control-dark" placeholder="Search..." aria-label="Search">
-        </form>
+            <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
+                <input type="search" class="form-control form-control-dark" placeholder="Search..." aria-label="Search">
+            </form>
 
-        <div class="text-end">
-          <button type="button" class="btn btn-outline-light me-2">Login</button>
-          <button type="button" class="btn btn-warning">Sign-up</button>
+            <div class="text-end">
+                <button type="button" class="btn btn-outline-light me-2">Login</button>
+                <button type="button" class="btn btn-warning">Sign-up</button>
+            </div>
         </div>
-      </div>
     </div>
-  </header> 
+</header>
