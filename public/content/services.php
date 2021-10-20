598fb1cafa523950278da4b3316abc3404393bc9
YS67Deepak <ys6727431@gmail.com>
2021-10-20 13:18:58 +0530

    added breadcrum

--- public/content/services.php
+++ public/content/services.php
@@ -6,14 +6,16 @@
     <div class="col-sm-6 col-lg-4 mb-4   ">
 
 
-        <h2 style="    text-align: end;  margin-right: -50%"><a
-                href="http://localhost/first_repository/public/about.php">About-Us</a>
+        <h2 style="    text-align: end;  margin-right: -50%">
+            <a href="http://localhost/first_repository/public/index.php">Home</a>
+
             <i class="bi bi-chevron-right"></i><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
                 fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
                 <path fill-rule="evenodd"
                     d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z" />
             </svg>
-            <a href="http://localhost/first_repository/public/index.php">Home</a>
+
+            <a href="http://localhost/first_repository/public/about.php">About-Us</a>
         </h2>
     </div>
     </div>
